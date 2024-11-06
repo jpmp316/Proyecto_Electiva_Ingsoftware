d0704f6836bd2ecd91748123daf5361fddb7489e
jpmp316 <61028986+jpmp316@users.noreply.github.com>
2024-11-06 18:01:13 +0000

    principal

---
+++ vistas/principal/index.php
@@ -0,0 +1,235 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Boudin</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="../../assets/css/main.css" />
+		<link rel="icon" type="image/png" href="../../images/captura.png">
+	</head>
+	<body class="homepage is-preload">
+		
+		<div id="page-wrapper">
+			<!-- Header -->
+				<section id="header">
+					<img src="../../images/Boudinnn.png" alt="" width="250" >
+					
+					<!-- Logo -->
+						<!-- <h1><a href="index.php">Boudin </a></h1> -->
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li class="current"><a href="index.php">Inicio</a></li>
+								<li><a href="#main">Temas</a></li>
+								<li><a href="#footer">Contactos</a></li>
+								<li><a href="../login/loginYregistro.php" >Iniciar sesión</a></li>
+							</ul>
+						</nav>
+						
+					<!-- Banner -->
+					<section id="banner">
+							<header>
+								<h2>Hola. Esto es Boudin.</h2>
+									<p style="max-width: 800px;">Un proyecto de investigacíon que se propone indagar a fondo en los factores que pueden influir en la comprensión lectora de los estudiantes de undécimo grado en dicha institución.</p>
+								</header>
+						</section>
+
+					<!-- Intro -->
+						<section id="intro" class="container">
+							<div class="row">
+								<div class="col-4 col-12-medium">
+									<section class="first">
+										<i class="icon solid featured fa-cog"></i>
+										<header>
+											<h2>Configura Tu Viaje Literario</h2>
+										</header>
+										<p>Ajusta la velocidad, elige el formato y haz que cada momento de lectura sea único para ti.</p>
+									</section>
+								</div>
+								<div class="col-4 col-12-medium">
+									<section class="middle">
+										<i class="icon solid featured alt fa-bolt"></i>
+										<header>
+											<h2>Enciende tu Curiosidad</h2>
+										</header>
+										<p>Sigue el camino iluminado por el rayo, explorando lecturas estimulantes que despierten tu mente y desafíen tus límites.</p>
+									</section>
+								</div>
+								<div class="col-4 col-12-medium">
+									<section class="last">
+										<i class="icon solid featured alt2 fa-star"></i>
+										<header>
+											<h2>Colecciona Estrellas Literarias</h2>
+										</header>
+										<p>Cada estrella representa una historia que te ha cautivado. Deja que tu colección brille con historias que han dejado una huella en tu corazón.</p>
+									</section>
+								</div>
+							</div>
+							<footer>
+								<ul class="actions">
+									<li><a href="../login/loginYregistro.php" class="button large">Iniciar sesión</a></li>
+									<li><a href="../login/loginYregistro.php"  class="button alt large">Registrarme</a></li>
+								</ul>
+							</footer>
+						</section>
+
+				</section>
+
+			<!-- Main -->
+				<section id="main">
+					<div class="container">
+						<div class="row">
+							<div class="col-12">
+
+								<!-- Portfolio -->
+									<section>
+										<header class="major">
+											<h2>Temas</h2>
+										</header>
+										<div class="row">
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="../../images/inferencia.jpg" alt="" height="208" /></a>
+													<header>
+														<h3>Inferencia y Deducción</h3>
+													</header>
+													<p>La habilidad para extraer información implícita en el texto y hacer deducciones basadas en pistas contextuales.</p>
+													
+												</section>
+											</div>
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="../../images/argument.png" alt="" height="180" /></a>
+													<header>
+														<h3>Análisis de Texto Argumentativo</h3>
+													</header>
+													<p>Comprender y evaluar argumentos presentados en un texto, identificar la tesis y evaluar la validez de las afirmaciones.</p>
+													
+												</section>
+											</div>
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="../../images/literarias.jpg" alt="" height="180" /></a>
+													<header>
+														<h3>Interpretación de Figuras Literarias</h3>
+													</header>
+													<p>Analizar y comprender elementos literarios como metáforas, simbolismos y alegorías presentes en textos narrativos.</p>
+													
+												</section>
+											</div>
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="../../images/textx.jpg" alt="" height="233" /></a>
+													<header>
+														<h3>Comprensión de Textos Científicos</h3>
+													</header><br>
+													<p>Entender textos científicos, técnicos o académicos, interpretando datos, gráficos y conceptos especializados.</p>
+													
+												</section>
+											</div>
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="../../images/context.jpg" alt="" height="233" /></a>
+													<header>
+														<h3>Contextualización Histórica</h3>
+													</header><br>
+													<p> Relacionar información leída con el contexto histórico, comprendiendo eventos, personajes y su relevancia en un período determinado.</p>
+													
+												</section>
+											</div>
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="../../images/teorias.jpg" alt="" height="233"/></a>
+													<header>
+														<h3>Crítica y Evaluación de Medios</h3>
+													</header>
+													<p>Desarrollar la capacidad de leer críticamente noticias, artículos y otros contenidos, evaluando la objetividad y la veracidad de la información.</p>
+													
+												</section>
+											</div>
+										</div>
+									</section>
+
+							</div>
+							
+						</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+				<section id="footer">
+					<div class="container">
+						<div class="row">
+							
+							<div class="col-8 col-12-medium">
+								<section>
+									<header>
+										<h2>Consejos para Mejorar la Comprensión Lectora</h2>
+									</header>
+									<ol>
+										<li><strong>Lee regularmente:</strong> Practica la lectura con una variedad de materiales.</li>
+										<li><strong>Selecciona lecturas apropiadas:</strong> Escoge textos que te desafíen un poco.</li>
+										<li><strong>Haz preguntas antes de leer:</strong> Formula preguntas para enfocarte.</li>
+										<li><strong>Subraya o toma notas:</strong> Destaca ideas clave y toma notas mientras lees.</li>
+										<li><strong>Revisa el vocabulario:</strong> Busca significados de palabras desconocidas.</li>
+										<li><strong>Identifica la estructura del texto:</strong> Reconoce la organización del contenido.</li>
+										<li><strong>Lee en voz alta:</strong> Mejora la procesamiento y retención.</li>
+										<li><strong>Establece metas de lectura:</strong> Define objetivos claros antes de empezar.</li>
+										<li><strong>Revisa y resume:</strong> Resumen al finalizar para reforzar la comprensión.</li>
+										<li><strong>Practica la lectura crítica:</strong> Cuestiona la información y desarrolla pensamiento crítico.</li>
+										<li><strong>Busca retroalimentación:</strong> Comparte reflexiones y busca feedback externo.</li>
+									</ol>
+								</section>
+							</div>
+							
+							<div class="col-4 col-12-medium">
+								<section>
+									<header>
+										<h2>Contactos</h2>
+									</header>
+									
+									<ul class="contact">
+										<li>
+											<h3>Dirección</h3>
+											<p>
+												Sinelejo - Sucre<br />
+											</p>
+										</li>
+										<li>
+											<h3>Correos</h3>
+											<p><a href="#">Ana.montesm@cecar.edu.co</a></p>
+											<p><a href="#">Bianeys@cecar.edu.co</a></p>
+										</li>
+										
+									</ul>
+								</section>
+							</div>
+							<div class="col-12">
+
+								<!-- Copyright -->
+									<div id="copyright">
+										<ul class="links">
+											<li>&copy; Boudin. Todos los derechos reservados.</li>
+										</ul>
+									</div>
+
+							</div>
+						</div>
+					</div>
+				</section>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="../../assets/js/jquery.min.js"></script>
+			<script src="../../assets/js/jquery.dropotron.min.js"></script>
+			<script src="../../assets/js/browser.min.js"></script>
+			<script src="../../assets/js/breakpoints.min.js"></script>
+			<script src="../../assets/js/util.js"></script>
+			<script src="../../assets/js/main.js"></script>
+			
+
+	</body>
+</html>
